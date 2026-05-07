51db6bdfeeb2e0823673606340bef35f401c719d
bdot100 <bidemiokunade@gmail.com>
2026-05-07 13:08:20 +0100

    feat: integrate react-hook-form and react-simplemde-editor to enable new issue creation with form handling

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,16 +1,43 @@
-import { Button, TextArea, TextField } from '@radix-ui/themes'
-import React from 'react'
+'use client'
+
+import dynamic from "next/dynamic";
+import { Button, TextField } from '@radix-ui/themes';
+import { Controller, useForm } from 'react-hook-form';
+import axios from "axios";
+import "easymde/dist/easymde.min.css";
+import { useRouter } from "next/navigation";
+
+const SimpleMDE = dynamic(() => import("react-simplemde-editor"), {
+    ssr: false,
+})
+
+interface IssueForm {
+    title: string;
+    description: string;
+}
 
 const NewIssuePage = () => {
+    const router = useRouter()
+    const { register, handleSubmit, control } = useForm<IssueForm>();
+
     return (
-        <div className='max-w-xl space-y-3'>
-            <TextField.Root radius="none" placeholder="Title of the Issue" />
-            <TextArea placeholder="Description of the Issue" />
+        <form
+            className='max-w-xl space-y-3'
+            onSubmit={handleSubmit(async (data) => {
+                await axios.post('/api/issues', data);
+                router.push('/issues');
+            })}>
+            <TextField.Root radius="none" placeholder="Title of the Issue" {...register("title")} />
+            <Controller
+                control={control}
+                name="description"
+                render={({ field }) => <SimpleMDE placeholder="Description of the Issue" {...field} />}
+            />
 
             <Button>
-                Submit New Issue
+                Create Issue
             </Button>
-        </div>
+        </form>
     )
 }
 
